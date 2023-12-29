d288eadd17afa53029900424af97059f23450292
Goose <80514798+GUSBLET@users.noreply.github.com>
2023-12-29 20:30:52 +0100

    add dropdown manu

--- src/main/java/com/source/loader/model3d/Model3DService.java
+++ src/main/java/com/source/loader/model3d/Model3DService.java
@@ -129,7 +129,7 @@ public class Model3DService {
         model3D.setBrand(brandService.updateBrand(dto.getBrand()));
         model3DRepository.updateModel3DById(model3D.getId(), model3D.getName(), model3D.getDescription(),
                 model3D.getLowPolygonPath(), model3D.getHighPolygonPath(), model3D.getBackgroundPath(),
-                model3D.getBrand(), model3D.getPriority());
+                model3D.getBrand());
     }
 
     private void capitalizeMainVariables(@NotNull Model3dCreatingDTO dto) {
