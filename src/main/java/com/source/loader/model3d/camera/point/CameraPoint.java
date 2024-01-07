2eeecc72f5d8cd0a3b0f168b7bfa88e6364535ec
Goose <80514798+GUSBLET@users.noreply.github.com>
2024-01-07 02:44:14 +0100

    updating color descrpiption fix

--- src/main/java/com/source/loader/model3d/camera/point/CameraPoint.java
+++ src/main/java/com/source/loader/model3d/camera/point/CameraPoint.java
@@ -25,23 +25,23 @@ public class CameraPoint {
     @Column(name = "description", columnDefinition = "varchar(500)")
     private String description;
 
-    @Column(name = "point_x_position", columnDefinition = "float")
-    private Float point_x_position;
+    @Column(name = "point_x_position", columnDefinition = "double precision")
+    private Double point_x_position;
 
-    @Column(name = "point_y_position", columnDefinition = "float")
-    private Float point_y_position;
+    @Column(name = "point_y_position", columnDefinition = "double precision")
+    private Double point_y_position;
 
-    @Column(name = "point_z_position", columnDefinition = "float")
-    private Float point_z_position;
+    @Column(name = "point_z_position", columnDefinition = "double precision")
+    private Double point_z_position;
 
-    @Column(name = "camera_x_position", columnDefinition = "float")
-    private Float camera_x_position;
+    @Column(name = "camera_x_position", columnDefinition = "double precision")
+    private Double camera_x_position;
 
-    @Column(name = "camera_y_position", columnDefinition = "float")
-    private Float camera_y_position;
+    @Column(name = "camera_y_position", columnDefinition = "double precision")
+    private Double camera_y_position;
 
-    @Column(name = "camera_z_position", columnDefinition = "float")
-    private Float camera_z_position;
+    @Column(name = "camera_z_position", columnDefinition = "double precision")
+    private Double camera_z_position;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "model_id")
